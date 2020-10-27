82e96d5ee0499c969e76b3740c62406a16056345
jbrown34 <jbrown34@chicagobooth.edu>
2020-10-27 03:17:30 +0000

    100% complete

--- app/controllers/actor_controller.rb
+++ app/controllers/actor_controller.rb
@@ -3,4 +3,12 @@ class ActorController < ApplicationController
     @actor_list = Actor.all
     render({ :template => "/actor_pages/index.html.erb"})
   end
+
+  def actor_details
+    id = params.fetch("actor_id")
+    @actor = Actor.all.where({ :id => "#{id}"}).at(0)
+    @credits = Character.all.where({ :actor_id => id})
+        render({ :template => "/actor_pages/actor_bio.html.erb"})
+  end
+
 end
